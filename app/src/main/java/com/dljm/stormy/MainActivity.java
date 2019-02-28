e9ca960d3ebdb971092cfbb5440a934ccad54ca4
DillonPC <xpoweredbyasusx@gmail.com>
2019-02-28 13:41:22 -0500

    Added location services and finished the app for now.

--- app/src/main/java/com/dljm/stormy/MainActivity.java
+++ app/src/main/java/com/dljm/stormy/MainActivity.java
@@ -1,10 +1,17 @@
 package com.dljm.stormy;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
@@ -12,13 +19,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.dljm.stormy.databinding.ActivityMainBinding;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -34,18 +48,131 @@ public class MainActivity extends AppCompatActivity {
     private ImageView iconImageView;
     private ActivityMainBinding binding;
 
-    final double latitude = 46.3091;
-    final double longitude = -79.4608;
+    private FusedLocationProviderClient fusedLocationClient;
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
+    private Location mLastLocation;
+    //updated dynamically
+    private double latitude = 46.3091;
+    private double longitude = -79.4608;
+    private String locality = "North Bay";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set up location services
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         //set up data binding class for activity_main.xml
         binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
         //on app startup show a loading screen for first weather request
         findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
         getForecast(latitude, longitude);
     }//end onCreate
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            getLastLocation();
+        }
+    }//end onStart
+
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            alertUserAboutError(getString(R.string.permission_rationale));
+            startLocationPermissionRequest();
+
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            startLocationPermissionRequest();
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation();
+            } else {
+                // Permission denied.
+                // Notify the user that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                alertUserAboutError(getString(R.string.permission_rationale));
+            }
+        }
+    }//end onRequestPermission
+
+    /**
+     * Provides a simple way of getting a device's location and is well suited for
+     * applications that do not require a fine-grained location and that do not need location
+     * updates. Gets the best and most recent location currently available, which may be null
+     * in rare cases when a location is not available.
+     * <p>
+     * Note: this method should be called after location permission has been granted.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            mLastLocation = task.getResult();
+                            latitude = mLastLocation.getLatitude();
+                            longitude = mLastLocation.getLongitude();
+
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception", task.getException());
+                            alertUserAboutError("No Location Detected.");
+                        }
+                    }
+                });
+    }
+
     /*
         Helper Functions for onCreate method
      */
@@ -91,7 +218,6 @@ public class MainActivity extends AppCompatActivity {
                                 public void run() {
                                     Drawable drawable = getResources().getDrawable(currentWeather.getIconId());
                                     iconImageView.setImageDrawable(drawable);
-                                    //findViewById(R.id.loadingPanel).setVisibility(View.GONE);
                                 }
                             });
                         } else {
@@ -99,12 +225,12 @@ public class MainActivity extends AppCompatActivity {
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "IO Exception caught:", e);
-                        alertUserAboutError("The weather data took too long to load, please tap refresh to try again.");
+                        alertUserAboutError(getString(R.string.timeout_error));
                     } catch(JSONException e){
                         Log.e(TAG, "JSON Exception caught:", e);
                         alertUserAboutError(getString(R.string.error_message));
                     }
-                    //hide loading circles
+                    //hide loading circles, must be done on main thread
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -136,8 +262,20 @@ public class MainActivity extends AppCompatActivity {
         currentWeather.setSummary(currently.getString("summary"));
         currentWeather.setTemperature(currently.getDouble("temperature"));
         currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
-        //TODO add location dynamically
-        currentWeather.setLocationLabel("North Bay, ON");
+
+        //dynamically get the city name
+        Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            //could be improved by running on different thread
+            addresses = gcd.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses != null && addresses.size() > 0) {
+            locality = addresses.get(0).getLocality();
+        }
+        currentWeather.setLocationLabel(locality);
 
         return currentWeather;
     }//end getCurrentDetails
@@ -171,6 +309,11 @@ public class MainActivity extends AppCompatActivity {
     //adds button functionality to refresh icon in UI
     public void refreshOnClick(View view){
         findViewById(R.id.refreshLoad).setVisibility(View.VISIBLE);
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            getLastLocation();
+        }
         getForecast(latitude, longitude);
     }//end refreshOnClick
 }//end MainActivity
